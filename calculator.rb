e8cb4601082ed198f10b10adbf6bfba57b21df10
Chris Martin <bmg.martin08@gmail.com>
2020-10-21 20:41:58 +0000

    Automatically backed up by Learn

--- calculator.rb
+++
@@ -1,7 +0,0 @@
-first_number = 100
-second_number = 20
-sum = first_number + second_number
-difference = first_number - second_number
-product = first_number * second_number
-quotient = first_number / second_number
-# Add your variables here
